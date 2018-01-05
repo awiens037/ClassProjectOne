c496d18e471cd76ced0d4123035bdac3e110f01d
VRKnyght <VRKnyght@gmail.com>
2018-01-04 20:07:50 -0800

    Added face++ api call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,3 +9,28 @@ $('button').on('click', function() {
 $('button').on('click', function() {
 		$('.container5').show();
 	});
+
+
+// Set up the API key in the URL.
+var apiKey = '_FBOOV2kaZLXlIQUHIsgdAiflLG50';
+
+// Add the secret to the URL.
+var apiSecret = 'vDa7HQqq9D-Wz6QiRjlIgm_VSfnfQ1Iz';
+
+// We then take the image and add it to the URL.
+var image = '';
+
+// This is the finished URL
+var queryURL = 'https://api-us.faceplusplus.com/facepp/v3/detect/?api_key=' + apiKey + '&api_secret=' + apiSecret + '&image_file=' + image + '&return_attributes=emotion';
+
+// Once the image has been added, we then call the Face++ api with the URL to get the emotions from the image
+$.ajax({
+	url: queryURL,
+	method: "POST",
+}).done(function(response){
+console.log(response.faces.attributes.emotion)
+})
+
+// Determine which emotion is the most apparent and add search terms to the Youtube search params.
+if 
+sadness, neutral, disgust, anger, surprise, fear, happiness.
