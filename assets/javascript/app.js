5d7e1f08ea0e1ead1b1c3ed88114997e4f884cc6
VRKnyght <VRKnyght@gmail.com>
2018-01-09 19:34:27 -0800

    took out container 5

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,16 +3,11 @@ $(document).ready(function () {
 
 	//following code hides container4 and 5//
 	$('.container4').hide();
-	$('.container5').hide();
 
 	//following code displays container4 and 5 when button clicked//
 	$('button').on('click', function() {
 			$('.container4').show();
 		});
-	$('button').on('click', function() {
-			$('.container5').show();
-		});
-
 	$('.emotion').on('click', function(){
 		searchTerm = 'music playlist -vevo -wmg ';
 	})
